a0756c7223328b5fc6901b4c06dcc6dd27890108
Christopher Price <christopher.r.price@gmail.com>
2019-12-16 23:19:53 -0500

    Inital commit with basic UI

---
+++ DistanceConverter/DistanceConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DistanceConverter
+//
+//  Created by Chris Price on 12/16/19.
+//  Copyright © 2019 Chris Price. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
